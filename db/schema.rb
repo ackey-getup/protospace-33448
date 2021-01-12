cb1227b8d290daddac982dbbc084fbcdd5285ad4
ackey-getup <akira.getup@gmail.com>
2021-01-12 13:33:37 +0900

    commentsテーブルのマイグレーション修正の差し戻し

--- db/schema.rb
+++ db/schema.rb
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2021_01_08_091440) do
   end
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.text "comment", null: false
+    t.text "text", null: false
     t.bigint "user_id", null: false
     t.bigint "prototype_id", null: false
     t.datetime "created_at", precision: 6, null: false
