39812bc07c7bbf4e681cfc5741acaf8b877947c1
ackey-getup <akira.getup@gmail.com>
2021-01-12 12:38:22 +0900

    コメント投稿機能の修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,9 @@
 class CommentsController < ApplicationController
 
   def create
-    @comment = Comment.new(comment_params)
-    if @comment.save
-      redirect_to prototype_path(@comment.prototype)
+    comment = Comment.new(comment_params)
+    if comment.save
+      redirect_to prototype_path(comment.prototype)
     else
       @prototype = @comment.prototype
       @comments = @prototype.comments
